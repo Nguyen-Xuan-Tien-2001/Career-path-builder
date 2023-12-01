3e10246f39d03dd327b4b3269fa77c7fc8e28021
20120519 <lieunguyenthi1802@gmail.com>
2023-12-01 08:48:28 +0700

    commit tạm

--- src/components/Rout/rout.tsx
+++ src/components/Rout/rout.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Map from "../Map/map";
+import StaffReview from "../StaffReview/staffReview";
 
 function Rout() {
     return (
@@ -8,7 +9,7 @@ function Rout() {
                 <Route path={'/'} element={<Map/>}/>
                 <Route path={'/so-sanh-nhan-vien'} />
                 <Route path={'/thong-tin-nhan-vien-danh-gia'}/>
-                <Route path={'/danh-gia-nhan-vien'}/>
+                <Route path={'/danh-gia-nhan-vien'} element={<StaffReview/>}/>
                 <Route path={'/khai-bao-dot-danh-gia'}/>
                 <Route path={'/khai-bao-dot-danh-gia/chi-tiet-dot-danh-gia'}/>
             </Routes>
---
+++ src/components/StaffReview/staffReview.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'antd';
+
+import "./staffReview.css";
+
+function StaffReview() {
+    return (<>
+        <div className="staff_review-root">
+            <div className="staff_info-container">
+                <div className="staff_info-text">
+                    Đánh giá nhân viên
+                </div>
+                <div className='staff_info-select'>
+                    <div>
+                        Nhân viên được đánh giá
+                    </div>
+                    <div>
+                        Selectbox
+                    </div>
+                </div>
+                <div>
+                    <Button type="primary">Chi tiết bài đánh giá</Button>
+                </div>
+            </div>
+            <div className="staff_review-container">
+
+            </div>
+        </div>
+    </>);
+}
+
+export default StaffReview;
